929e61df03c901347c29ef637f95d4dc50f7173f
EMLGaming <ezrah@ligthartschenk.com>
2018-05-20 19:22:17 +0200

    working on it

--- permutationgen.cpp
+++ permutationgen.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main(){
     
-    
+    //working on it go ahead and try to solve
+    //try to make a permutation generator so all the possible combinations with an array of integers.
 
 }
